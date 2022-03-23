8ef0074423bea82bb3eb2caf1fbfedcebd61a367
merong245 <37497183+merong245@users.noreply.github.com>
2022-03-23 17:22:51 +0900

    달성과제 추가구현
    기존 달성과제 목록만 넘김에서
    달성 일자 추가
    
    성취과제 레포지에 달성일자 추가

--- web/backend/src/main/java/com/edu/opensky/achievement/Achievement.java
+++ web/backend/src/main/java/com/edu/opensky/achievement/Achievement.java
@@ -7,27 +7,35 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor
 @Entity
 public class Achievement {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
     private Long achievementId;
+
+
+    private String name;
+    private Long experience;
+    private LocalDate date;
 
     @JsonBackReference
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "mte_id")
     private Mentee mentee;
-    private String name;
-    private Long experience;
 
     @Builder
-    public Achievement(Mentee mentee, String name, Long experience){
+    public Achievement(Long achievementId, Mentee mentee, String name, Long experience){
+        this.achievementId = achievementId;
         this.mentee = mentee;
         this.name = name;
         this.experience = experience;
+        this.date = LocalDate.now();
 
     }
 }
--- web/backend/src/main/java/com/edu/opensky/achievement/AchievementService.java
+++ web/backend/src/main/java/com/edu/opensky/achievement/AchievementService.java
@@ -1,10 +1,12 @@
 package com.edu.opensky.achievement;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.jni.Local;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import javafx.util.Pair;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,8 +14,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -22,9 +27,9 @@ public class AchievementService {
     private final AchievementRepository achievementRepository;
     private final AchieveListRepository achieveListRepository;
 
-    // 학생 아이디로 달성한 과제 번호 리스트 반환
-    public List<Long> getAchievementList(String stdId){
-        return achievementRepository.findIdByMentee_MteId(stdId);
+    // 학생 아이디로 달성한 과제 번호와 달성일자 리스트 반환
+    public JSONArray getAchievementList(String stdId){
+        return convertListToJson(achievementRepository.findIdByMentee_MteId(stdId));
 
     }
 
@@ -47,10 +52,6 @@ public class AchievementService {
                 achieveListRepository.save(AchieveList.builder().id(id).title(title).experience(exp).build());
             }
             
-
-
-
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -59,5 +60,22 @@ public class AchievementService {
             e.printStackTrace();
         }
 
+    }
+
+    public static JSONArray convertListToJson(List<HashMap<Long, LocalDate>> bankCdList) {
+        JSONArray jsonArray = new JSONArray();
+        for (Map<Long, LocalDate> map : bankCdList) {
+            jsonArray.add(convertMapToJson(map));
+        }
+        return jsonArray;
+    }
+    public static JSONObject convertMapToJson(Map<Long, LocalDate> map) {
+        JSONObject json = new JSONObject();
+        for (Map.Entry<Long, LocalDate> entry : map.entrySet()) {
+            String key = String.valueOf(entry.getKey());
+            Object value = entry.getValue();
+            json.put(key, value);
+        }
+        return json;
     }
 }
