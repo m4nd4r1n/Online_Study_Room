4eddc1a6603bde3fdf143bdbfee8a1d7e8d78cba
merong245 <37497183+merong245@users.noreply.github.com>
2022-04-24 22:23:58 +0900

    현재 학습중 인원, 오늘 학습 인원
    현재 학습은 start time만 있는 경우,
    오늘 학습은 오늘 start time이 있는 경우

--- web/backend/src/main/java/com/edu/opensky/studytime/StudyTimeRepository.java
+++ web/backend/src/main/java/com/edu/opensky/studytime/StudyTimeRepository.java
@@ -20,7 +20,7 @@ public interface StudyTimeRepository extends JpaRepository<StudyTime, Long> {
     @Transactional(readOnly = true)
     @Query(value = "select m.school as school, u.name as name, sec_to_time(sum(time_to_sec(s.endTime)-time_to_sec(s.startTime))) as time " +
             "from StudyTime s join User u on s.mte_id = u.email join Mentee m on m.mteId = s.mte_id " +
-            "where date(s.startTime) = date(:date) " +
+            "where date(s.startTime) = date(:date) and not isnull(s.endTime) " +
             "group by s.mte_id " +
             "order by time desc ",nativeQuery = true)
     Page<timeRankingListInterface> findByRankingOfTimeForDay(LocalDate date, Pageable pageable);
@@ -28,7 +28,7 @@ public interface StudyTimeRepository extends JpaRepository<StudyTime, Long> {
     @Transactional(readOnly = true)
     @Query(value = "select m.school as school,u.name as name, sec_to_time(sum(time_to_sec(s.endTime)-time_to_sec(s.startTime))) as time " +
             "from StudyTime s join User u on s.mte_id = u.email join Mentee m on m.mteId = s.mte_id " +
-            "where datediff(date(s.startTime), :date) < 7 " +
+            "where datediff(date(s.startTime), :date) < 7 and not isnull(s.endtime) " +
             "group by s.mte_id " +
             "order by time desc ",nativeQuery = true)
     Page<timeRankingListInterface> findByRankingOfTimeForWeek(LocalDate date,Pageable pageable);
@@ -36,8 +36,22 @@ public interface StudyTimeRepository extends JpaRepository<StudyTime, Long> {
     @Transactional(readOnly = true)
     @Query(value = "select u.name as name,m.school as school, sec_to_time(sum(time_to_sec(s.endTime)-time_to_sec(s.startTime))) as time " +
             "from StudyTime s join User u on s.mte_id = u.email join Mentee m on m.mteId = s.mte_id " +
-            "where month(s.startTime)= month(:date) and year(s.startTime) = year(:date) " +
+            "where month(s.startTime)= month(:date) and year(s.startTime) = year(:date) and not isnull(s.endtime) " +
             "group by s.mte_id " +
             "order by time desc ",nativeQuery = true)
     Page<timeRankingListInterface> findByRankingOfTimeForMonth(LocalDate date,Pageable pageable);
+
+    @Transactional(readOnly = true)
+    @Query(value = "select count(distinct(s.mte_id)) " +
+            "from StudyTime s " +
+            "where month(s.startTime)= month(:date) and year(s.startTime) = year(:date) "
+            ,nativeQuery = true)
+    Integer countByStartTime(LocalDate date);
+
+    @Transactional(readOnly = true)
+    @Query(value = "select count(distinct(s.mte_id)) " +
+            "from StudyTime s " +
+            "where month(s.startTime)= month(:date) and year(s.startTime) = year(:date) and isnull(s.endTime)"
+            ,nativeQuery = true)
+    Integer countByStartTimeAndEndTimeIsNull(LocalDate date);
 }
