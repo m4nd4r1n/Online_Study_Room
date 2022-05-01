bafada489f77aa48aa5a26d8028cb4bc84b15ca5
merong245 <37497183+merong245@users.noreply.github.com>
2022-05-02 03:33:24 +0900

    멘티리스트 반환
    멘티 아이디 리스트를 반환합니다.

--- web/backend/src/main/java/com/edu/opensky/user/mentee/MenteeRepository.java
+++ web/backend/src/main/java/com/edu/opensky/user/mentee/MenteeRepository.java
@@ -3,10 +3,14 @@ package com.edu.opensky.user.mentee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MenteeRepository extends JpaRepository<Mentee,String> {
 
     @Transactional(readOnly = true)
     Optional<Mentee> findByMteId(String mteId);
+
+    @Transactional(readOnly = true)
+    List<Mentee> findByMtrId(String mtrId);
 }
--- web/backend/src/main/java/com/edu/opensky/user/mentor/MentorApiController.java
+++ web/backend/src/main/java/com/edu/opensky/user/mentor/MentorApiController.java
@@ -1,7 +1,11 @@
 package com.edu.opensky.user.mentor;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,7 +14,16 @@ public class MentorApiController {
 
     private final MentorService mentorService;
 
+    @GetMapping("/menteeList")
+    public List<String> getMenteeList(){
+        // 수정 필요
+        // 멘토 아이디
+        String id= "dsadsa@naver.com";
 
+        // 멘티 아이디 리스트 반환
+        return mentorService.getMenteeList(id);
+
+    }
 
 
 }
