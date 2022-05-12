cd2e5b22d24c2d7f6b834b05df62052e844627ec
m4nd4r1n <kara.mandarin99@gmail.com>
2022-05-12 23:37:30 +0900

    랭킹 오류 수정
    랭킹 탭 빠르게 변경 시 제대로 데이터 불러오지 못하는 오류 수정

--- web/frontend/src/containers/ranking/RankingContainer.js
+++ web/frontend/src/containers/ranking/RankingContainer.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Ranking from '../../components/ranking/Ranking';
+import { getRanking } from '../../lib/api/ranking';
 import useSWR from 'swr';
 
 const RankingContainer = () => {
-  const { data } = useSWR('/api/rank');
-
+  const { data } = useSWR('/api/rank', getRanking);
   return <Ranking ranks={data} />;
 };
 
