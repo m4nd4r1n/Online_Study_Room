d7d11b118e33612abaca9313aaedf0b782c7d82d
merong245 <37497183+merong245@users.noreply.github.com>
2022-05-15 02:37:54 +0900

    study/info 구현
    멘티리스트는 기존 함수 이용

--- web/backend/src/main/java/com/edu/opensky/user/mentee/MenteeService.java
+++ web/backend/src/main/java/com/edu/opensky/user/mentee/MenteeService.java
@@ -1,6 +1,7 @@
 package com.edu.opensky.user.mentee;
 
 import com.edu.opensky.user.mentee.dto.MenteeSaveRequestDto;
+import com.edu.opensky.user.mentee.dto.studyInfoResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +15,18 @@ public class MenteeService {
     @Transactional
     public String save(MenteeSaveRequestDto requestDto) {
         return menteeRepository.save(requestDto.toEntity()).getMtrId();
+    }
+
+    @Transactional(readOnly = true)
+    public studyInfoResponseDto getStudyInfo(String id){
+        Mentee mentee = menteeRepository.findByMteId(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("존재하는 아이디가 없습니다."));
+
+        return studyInfoResponseDto.builder()
+                .name(mentee.getName())
+                .exp(mentee.getExp())
+                .level(mentee.getLevel())
+                .build();
     }
 }
---
+++ web/backend/src/main/java/com/edu/opensky/user/mentee/dto/studyInfoResponseDto.java
@@ -0,0 +1,23 @@
+package com.edu.opensky.user.mentee.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class studyInfoResponseDto {
+    String name;
+    Integer level;
+    Integer exp;
+
+
+    @Builder
+    public studyInfoResponseDto(String name, Integer level, Integer exp){
+        this.name =name;
+        this.level = level;
+        this.exp = exp;
+    }
+
+
+}
