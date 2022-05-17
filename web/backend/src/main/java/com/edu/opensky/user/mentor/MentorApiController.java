ae51b60d723cfdce7a77c445e47a4c71421c49ac
merong245 <37497183+merong245@users.noreply.github.com>
2022-05-17 23:15:57 +0900

    플래너 추가 수정 및 getMenteeList 수정

--- web/backend/src/main/java/com/edu/opensky/user/mentor/MentorApiController.java
+++ web/backend/src/main/java/com/edu/opensky/user/mentor/MentorApiController.java
@@ -1,11 +1,15 @@
 package com.edu.opensky.user.mentor;
 
-import com.edu.opensky.user.mentee.Mentee;
+import com.edu.opensky.user.User;
+import com.edu.opensky.user.UserService;
+import com.edu.opensky.user.mentor.dto.MenteeListDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletRequest;
 import java.util.List;
 
 @RestController
@@ -14,15 +18,17 @@ import java.util.List;
 public class MentorApiController {
 
     private final MentorService mentorService;
+    private final UserService userService;
 
     @GetMapping("/menteeList")
-    public List<Mentee> getMenteeList(){
-        // 수정 필요
-        // 멘토 아이디
-        String id= "dsadsa@naver.com";
-
+    public List<MenteeListDto> getMenteeList(ServletRequest request){
+        userService.check(request);
+        Object principal= SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user= userService.getUserByToken(principal);
+        return mentorService.getMenteeList(user.getEmail());
         // 멘티 아이디 리스트 반환
-        return mentorService.getMenteeList(id);
+//        String id= "asdasd@naver.com";
+//        return mentorService.getMenteeList(id);
 
     }
 
