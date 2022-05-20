c3095c684d6010c5442f0bc1f882a310e6937abe
yerin1198 <yerin1198@naver.com>
2022-05-21 03:05:35 +0900

    statics 일부구현

--- web/backend/src/main/java/com/edu/opensky/statics/StaticsRepository.java
+++ web/backend/src/main/java/com/edu/opensky/statics/StaticsRepository.java
@@ -12,9 +12,14 @@ public interface StaticsRepository extends JpaRepository<StudyTime,Long> {
     @Override
     List<StudyTime> findAll();
 
+
+
+
+    //
     @Transactional(readOnly = true)
     @Query(value = "select sec_to_time(sum(time_to_sec(s.endTime)-time_to_sec(s.startTime))) as time " +
-            "from Studytime s join User u on s.mteId = u.email join Mentee m on m.mteId = s.mteId join Parent p on p.prtId = m.prtid ",nativeQuery = true)
-    public List<StaticsInterface> findByWeekStudyTime(String PtrId, LocalDateTime date);
+            "from Studytime s join User u on s.mteId = u.email join Mentee m on m.mteId = s.mteId join Parent p on p.prtId = m.prtid "+
+            "where datediff(date(s.startTime), :date) < 7 and not isnull(s.endtime) and p.prtId=:PtrId ",nativeQuery = true)
+    public List<? extends Object> findByWeekStudyTime(String PtrId, LocalDateTime date);
 
 }
