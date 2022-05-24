52733be31404ef4b0d51ff304fff70d8c99e45cf
merong245 <37497183+merong245@users.noreply.github.com>
2022-05-25 01:51:07 +0900

    /statistics 수정
    멘토, 학부모인 경우 추가

--- web/backend/src/main/java/com/edu/opensky/statics/StaticsController.java
+++ web/backend/src/main/java/com/edu/opensky/statics/StaticsController.java
@@ -1,7 +1,10 @@
 package com.edu.opensky.statics;
 
+import com.edu.opensky.user.User;
+import com.edu.opensky.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
@@ -13,6 +16,7 @@ import java.util.List;
 public class StaticsController {
 
     private final StaticsService staticsService;
+    private final UserService userService;
 
     @GetMapping("/weekStatic")
     public List<? extends Object> statics(@RequestBody StaticsRequestDto staticsRequestDto){
@@ -22,23 +26,40 @@ public class StaticsController {
     }
 
     @GetMapping("/statistics/week{query}")
-    public ResponseEntity getWeekStudyTime(@RequestParam @NotNull String userId,
+    public ResponseEntity getWeekStudyTime(@RequestParam @Nullable String userId,
                                            @RequestParam @NotNull String year,
                                            @RequestParam @NotNull String month,
-                                           @RequestParam @NotNull String day){
+                                           @RequestParam @NotNull String day,
+                                           @CookieValue(value="Authorization") String token){
+        User user = userService.getUserByToken(token);
+        if(user.getRole().equals("멘토") || user.getRole().equals("학부모")) {
 
-        // 분 단위
-        return ResponseEntity.ok(staticsService.getWeekStatics(userId,year,month,day));
+            // 분 단위
+            return ResponseEntity.ok(staticsService.getWeekStatics(userId, year, month, day));
+        }
+        else if(user.getRole().equals("멘티")){
 
+            // 분 단위
+            return ResponseEntity.ok(staticsService.getWeekStatics(user.getEmail(), year, month, day));
+
+        }
+        return ResponseEntity.badRequest().build();
     }
 
     @GetMapping("/statistics/date{query}")
-    public ResponseEntity getDateStudyTime(@RequestParam @NotNull String userId,
+    public ResponseEntity getDateStudyTime(@RequestParam @Nullable String userId,
                                            @RequestParam @NotNull String year,
                                            @RequestParam @NotNull String month,
-                                           @RequestParam @NotNull String day){
-        // 분 단위
-        return ResponseEntity.ok(staticsService.getDateStatics(userId,year,month,day));
-
+                                           @RequestParam @NotNull String day,
+                                           @CookieValue(value="Authorization") String token){
+        User user = userService.getUserByToken(token);
+        if(user.getRole().equals("멘토") || user.getRole().equals("학부모")) {
+            // 분 단위
+            return ResponseEntity.ok(staticsService.getDateStatics(userId, year, month, day));
+        }
+        else if (user.getRole().equals("멘티")){
+            return ResponseEntity.ok(staticsService.getDateStatics(user.getEmail(), year, month, day));
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
